ffd58d8ca9667cbfc645ddee571a9ded17de37eb
AllanHyr <djaleial@outlook.fr>
2024-07-17 09:15:17 +0200

    Changement pour ajouter la table batiments lors de l'appel API

--- pieceSalle.js
+++ pieceSalle.js
@@ -5,11 +5,13 @@ class PieceSalle {
         this._db = connection;
         this.table_1 = "pieces";
         this.table_2 = "salles";
+        this.table_3 = "batiments";
     }
  
     async getAll(params) {
         let pieces = [];
         let salles = [];
+        let batiments = [];
         let query = "SELECT * FROM ??";
         let queryParams = [
             this.table_1
@@ -37,12 +39,28 @@ class PieceSalle {
         result.forEach(entry => {
             let newPiece = {
                 id: entry.id,
-                title: entry.title
+                title: entry.title,
+                groupId: entry.fk_batiments
             }
             salles.push(newPiece);
         });
+        
+        query = "SELECT * FROM ??";
+        queryParams = [
+            this.table_3
+        ];
     
-        return { pieces, salles };
+        result = await this._db.query(query, queryParams);
+        
+        result.forEach(entry => {
+            let newBatiments = {
+                id: entry.id,
+                title: entry.title
+            }
+            batiments.push(newBatiments);
+        });
+    
+        return { pieces, salles, batiments };
     }
     
 }
